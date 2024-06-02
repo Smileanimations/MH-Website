21cc3311ba494035075aba75a9958409f9efa3f9
Mark <iksnij12@gmail.com>
2024-06-02 16:20:17 +0200

    ADDED EVERYTHING!

---
+++ Monster Hunter 1/index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<style></style>
+<body>
+<div class="row">
+    <div class="col text-center">
+        <div id="carousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-bs-interval="7000">
+                    <img src="./Images/Logos/Logo-MH1.webp"  width="28%" class="d-block mx-auto mt-3" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="7000">
+                    <img src="./Images/Logos/Logo-MHG.webp"  width="28%" class="d-block mx-auto mt-3" alt="...">
+                </div>
+                <div class="carousel-item" data-bs-interval="7000">
+                    <img src="./Images/Logos/Logo-MHP1.webp" width="26%" class="d-block mx-auto mt-3" alt="...">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    
+</div>
+
+</body>
+</html>
